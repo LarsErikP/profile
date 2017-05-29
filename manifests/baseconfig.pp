d763ca8f39f3529cb9fc2f63ed4e08ca6e5e1667
LarsErikP <pedersen.larserik@gmail.com>
2017-05-29 14:33:37 +0200

    Add base network config

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -1,5 +1,6 @@
 # This class ensures that all baseconfiguration are brought in.
 class profile::baseconfig {
   include ::profile::baseconfig::puppet
+  include ::profile::baseconfig::networking
   include ::profile::baseconfig::packages
 }
