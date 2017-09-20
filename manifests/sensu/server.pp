f5ea535ec3e61e7f56f3b79fc798bd770dcbc3f3
LarsErikP <pedersen.larserik@gmail.com>
2017-09-20 10:20:16 +0200

    Add keepalived to sensu and uchiwa

--- manifests/sensu/server.pp
+++ manifests/sensu/server.pp
@@ -1,10 +1,19 @@
 # Install and configure sensu-server and dashboard
 class profile::sensu::server {
   require ::profile::services::redis
+  require ::profile::services::keepalived
 
   $rabbithost = hiera('profile::rabbitmq::ip')
   $sensurabbitpass = hiera('profile::sensu::rabbit_password')
   $subs_from_client_conf = hiera('sensu::subscriptions','')
+
+  $vrrp_password = hiera('profile::keepalived::vrrp_password')
+  $vrid = hiera('profile::sensu::vrrp::id')
+  $vrpri = hiera('profile::sensu::vrrp::priority')
+
+  $management_if = hiera('profile::interfaces::management')
+
+  $sensu_ip = hiera('profile::sensu::vrrp::admin::ip')
 
   if ( $::is_virtual == 'true' ) {
     $subs = [ 'all' ]
@@ -46,6 +55,23 @@ class profile::sensu::server {
     attributes => {
       occurrences => "eval: value == 1 || ':::action:::' == 'resolve'",
     },
+  }
+
+  keepalived::vrrp::script { 'check_sensu':
+    script => '/usr/bin/killall -0 sensu-server',
+  }
+
+  keepalived::vrrp::instance { 'mgmt-sensu':
+    interface         => $management_if,
+    state             => 'MASTER',
+    virtual_router_id => $vrid,
+    priority          => $vrpri,
+    auth_type         => 'PASS',
+    auth_pass         => $vrrp_password,
+    virtual_ipaddress => [
+      "${sensu_ip}/32",
+    ],
+    track_script      => 'check_sensu',
   }
 #  include ::profile::sensu::checks
 #  include ::profile::sensu::plugins
--- manifests/sensu/uchiwa.pp
+++ manifests/sensu/uchiwa.pp
@@ -2,10 +2,19 @@
 class profile::sensu::uchiwa {
 
   require profile::services::apache
+  require profile::services::keepalived
 
   $password = hiera('profile::sensu::uchiwa::password')
   $api_name = 'sensu-lab'
   $uchiwa_url = hiera('profile::sensu::uchiwa::fqdn')
+
+  $vrrp_id = hiera('profile::uchiwa::vrrp::id')
+  $vrrp_priority = hiera('profile::uchiwa::vrrp::priority')
+  $vrrp_password = hiera('profile::keepalived::vrrp_password')
+
+  $management_if = hiera('profile::interfaces::management')
+
+  $uchiwa_ip = hiera('profile::uchiwa::vrrp::admin::ip')
 
   class { '::uchiwa':
     user                => 'sensu',
@@ -46,5 +55,22 @@ class profile::sensu::uchiwa {
     custom_fragment     => '
     ProxyHTMLEnable On
     ProxyHTMLURLMap http://127.0.0.1:3000/ /',
+  }
+
+  keepalived::vrrp::script { 'check_uchiwa':
+    script => '/usr/bin/killall -0 apache2',
+  }
+
+  keepalived::vrrp::script { 'mgmt-uchiwa':
+    interface         => $management_if,
+    state             => 'MASTER',
+    virtual_router_id => $vrrp_id,
+    priority          => $vrrp_priority,
+    auth_type         => 'PASS',
+    auth_pass         => $vrrp_password,
+    virtual_ipaddress => [
+      "${uchiwa_ip}/32",
+    ],
+    track_script      => 'check_uchiwa',
   }
 }
