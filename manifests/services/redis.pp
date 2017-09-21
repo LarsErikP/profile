2519b8fdc38b73fc6d0f719b7a0fad8ce6086279
LarsErikP <pedersen.larserik@gmail.com>
2017-09-21 14:24:47 +0200

    I am pretty stupid

--- manifests/services/redis.pp
+++ manifests/services/redis.pp
@@ -1,17 +1,17 @@
 # Install and configure standalone redis-server for sensu
 
-$nodetype = hiera('profile::redis::nodetype')
-$nic = hiera('profile::interfaces::management')
-$ip = $facts['networking'][$nic]['ip']
-$masterauth = hiera('profile::redis::masterauth')
-$redismaster = hiera('profile::redis::master')
-$slaveof = undef
-
-if ( $nodetype == 'slave' ) {
-  $slaveof = "${redismaster} 6379"
-}
-
 class profile::services::redis {
+  $nodetype = hiera('profile::redis::nodetype')
+  $nic = hiera('profile::interfaces::management')
+  $ip = $facts['networking'][$nic]['ip']
+  $masterauth = hiera('profile::redis::masterauth')
+  $redismaster = hiera('profile::redis::master')
+  $slaveof = undef
+
+  if ( $nodetype == 'slave' ) {
+      $slaveof = "${redismaster} 6379"
+  }
+
   class { '::redis':
     config_owner => 'redis',
     config_group => 'redis',
