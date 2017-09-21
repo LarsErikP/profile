81ff36041fe1c3db1c9c69846e480975ad0fbb76
LarsErikP <pedersen.larserik@gmail.com>
2017-09-21 14:14:05 +0200

    redis is now a cluster

--- manifests/services/redis.pp
+++ manifests/services/redis.pp
@@ -1,8 +1,27 @@
 # Install and configure standalone redis-server for sensu
+
+$nodetype = hiera('profile::redis::nodetype')
+$nic = hiera('profile::interfaces::management')
+$ip = $facts['networking'][$nic]['ip']
+$masterauth = hiera('profile::redis::masterauth')
+$redismaster = hiera('profile::redis::master')
+$slaveof = undef
+
+if ( $nodetype == 'slave' ) {
+  $slaveof = "${redismaster} 6379"
+}
+
 class profile::services::redis {
   class { '::redis':
     config_owner => 'redis',
     config_group => 'redis',
     manage_repo  => true,
+    bind         => $ip,
+    masterauth   => $masterauth,
+    slaveof      => $slaveof,
+  } ->
+
+  class { '::redis::sentinel':
+    redis_host => $redismaster,
   }
 }
