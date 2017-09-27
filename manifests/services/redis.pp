b7de7e54f29b6d0dd395ccb5f010cb7bc60276c2
LarsErikP <pedersen.larserik@gmail.com>
2017-09-27 09:16:01 +0200

    Add fw, bind to localhost, and set passwords correctly

--- manifests/services/redis.pp
+++ manifests/services/redis.pp
@@ -1,17 +1,24 @@
 # Install and configure standalone redis-server for sensu
 
 class profile::services::redis {
+
+  require ::firewall
+
   $nodetype = hiera('profile::redis::nodetype')
   $nic = hiera('profile::interfaces::management')
   $ip = $::networking['interfaces'][$nic]['ip']
-  $masterauth = hiera('profile::redis::masterauth')
+  $redispass = hiera('profile::redis::masterauth')
   $redismaster = hiera('profile::redis::master')
 
   if ( $nodetype == 'slave' ) {
     $slaveof = "${redismaster} 6379"
+    $masterauth = $redispass
+    $requirepass = undef
   }
   elsif ( $nodetype == 'master') {
     $slaveof = undef
+    $masterauth = undef
+    $requirepass = $redispass
   }
   else {
     fail('Wrong redis node type. Only master or slave are valid')
@@ -21,15 +28,26 @@ class profile::services::redis {
     config_owner        => 'redis',
     config_group        => 'redis',
     manage_repo         => true,
-    bind                => $ip,
+    bind                => "127.0.0.1 ${ip}",
     masterauth          => $masterauth,
     min_slaves_to_write => 1,
-    requirepass         => $masterauth,
+    requirepass         => $requirepass,
     slaveof             => $slaveof,
   } ->
 
   class { '::redis::sentinel':
     redis_host => $redismaster,
     auth_pass  => $masterauth,
+  }
+
+  firewall { '050 accept redis-server':
+    proto  => 'tcp',
+    dport  => 6379,
+    action => 'accept',
+  }
+  firewall { '051 accetp redis-sentinel':
+    proto  => 'tcp',
+    dport  => 26379,
+    action => 'accept',
   }
 }
