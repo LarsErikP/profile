936c840fcbcf8116864888972e527292cf85faf7
LarsErikP <pedersen.larserik@gmail.com>
2017-09-28 08:36:25 +0200

    Add sensu-client

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -1,4 +1,4 @@
-# This class ensures that all baseconfiguration are brought in.
+#This class ensures that all baseconfiguration are brought in.
 class profile::baseconfig {
   #  include ::profile::baseconfig::puppet
   include ::profile::baseconfig::networking
@@ -13,4 +13,9 @@ class profile::baseconfig {
   #}
   include ::profile::baseconfig::firewall_pre
   include ::profile::baseconfig::firewall_post
+
+  $installSensu = hiera('profile::sensu::install', true)
+    if ($::hostname !~ /^sensu/ and $installSensu) {
+      include ::profile::sensu::client
+    }
 }
