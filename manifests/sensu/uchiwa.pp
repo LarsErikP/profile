07c96bf5ef152cd8500708babf0e51544520c492
LarsErikP <pedersen.larserik@gmail.com>
2017-09-29 14:48:57 +0200

    Change access log env var

--- manifests/sensu/uchiwa.pp
+++ manifests/sensu/uchiwa.pp
@@ -30,6 +30,7 @@ class profile::sensu::uchiwa {
     port                => 80,
     docroot             => false,
     manage_docroot      => false,
+    access_log_env_var  => 'forwarded'
     proxy_preserve_host => true,
     proxy_pass          => [
       {
