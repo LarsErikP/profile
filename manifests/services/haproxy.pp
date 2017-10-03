bf7fe975867d40dd67ce4e8adecbd72864970ff3
LarsErikP <pedersen.larserik@gmail.com>
2017-10-03 09:47:56 +0200

    haproxy config for uchiwa

--- manifests/services/haproxy.pp
+++ manifests/services/haproxy.pp
@@ -1,5 +1,27 @@
 # Install and configure haproxy
 
 class profile::services::haproxy {
-  require ::haproxy
+  class { '::haproxy':
+    default_options => {
+      'option'  => [
+        'httplog',
+        'dontlognull',
+        'log-health-checks',
+        'redispatch',
+        'forwardfor',
+        'http-server-close',
+      ],
+      'timeout' => [
+        'connect 3s',
+        'server 6s',
+        'client 6s',
+      ],
+    },
+  }
+
+  $installSensu = hiera('profile::sensu::install')
+  if ( $installSensu ) {
+    include ::profile::services::haproxy::uchiwa
+  }
+
 }
---
+++ manifests/services/haproxy/uchiwa.pp
@@ -0,0 +1,20 @@
+# Haproxy config for uchiwa
+
+class profile::services::haproxy::uchiwa {
+
+  haproxy::frontend { 'ft_uchiwa':
+    ipaddress => '*',
+    ports     => '80,443',
+    mode      => 'http',
+    options   => {
+      'default_backend' => 'bk_uchiwa',
+    },
+  }
+
+  haproxy::backend { 'bk_uchiwa':
+    options => {
+      'balance' => 'source',
+      'cookie'  => 'SERVERID insert indirect nocache',
+    },
+  }
+}
