2a2d5d6a9afc08ae9ae6787b2a891342d35c895b
LarsErikP <pedersen.larserik@gmail.com>
2017-10-05 15:29:57 +0200

    linting

--- manifests/sensu/haproxy.pp
+++ manifests/sensu/haproxy.pp
@@ -5,9 +5,9 @@ class profile::sensu::haproxy {
   require ::profile::services::haproxy
 
   haproxy::defaults { 'uchiwa':
-    options  =>  {
-      'log'  => 'global',
-      'mode' => 'http',
+    options =>  {
+      'log'     => 'global',
+      'mode'    => 'http',
       'timeout' => [
         'connect 5s'.
         'server 50s',
@@ -21,17 +21,17 @@ class profile::sensu::haproxy {
   }
 
   haproxy::frontend { 'ft_uchiwa':
-    ipaddress           => '*',
-    ports               => '80,443',
-    defaults            => 'uchiwa',
-    options             => {
+    ipaddress => '*',
+    ports     => '80,443',
+    defaults  => 'uchiwa',
+    options   => {
       'default_backend' => 'bk_uchiwa',
     },
   }
 
   haproxy::backend { 'bk_uchiwa':
-    defaults    => 'uchiwa',
-    options     => {
+    defaults => 'uchiwa',
+    options  => {
       'balance' => 'source',
       'cookie'  => 'SERVERID insert indirect nocache',
     },
