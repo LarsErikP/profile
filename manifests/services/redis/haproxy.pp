40915976a2c3f8e06c15ddf987823f3c2d02aa4d
LarsErikP <pedersen.larserik@gmail.com>
2017-10-05 15:14:07 +0200

    Remove commented section

--- manifests/services/redis/haproxy.pp
+++ manifests/services/redis/haproxy.pp
@@ -49,25 +49,4 @@ class profile::services::redis::haproxy {
       ],
     },
   }
-
-    #  haproxy::listen { 'redis':
-    #ipaddress => '*',
-    #ports     => '6379',
-    #mode      => 'tcp',
-    #options   => {
-    #  'option'    => [
-    #    'tcp-check',
-    #    'tcplog',
-    #  ],
-    #  'tcp-check' => [
-    #    'connect',
-    #    'send PING\r\n',
-    #    'expect string +PONG',
-    #    'send info\ replication\r\n',
-    #    'expect string role:master',
-    #    'send QUIT\r\n',
-    #    'expect string +OK',
-    #  ],
-    #},
-    #}
 }
