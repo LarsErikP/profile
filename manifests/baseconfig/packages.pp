f8268ad423a6ecc0b6e22bcddf85c6d1f5dd62af
LarsErikP <pedersen.larserik@gmail.com>
2018-04-06 10:07:31 +0200

    use ensure_packages for base packages

--- manifests/baseconfig/packages.pp
+++ manifests/baseconfig/packages.pp
@@ -6,8 +6,7 @@ class profile::baseconfig::packages {
   $basepackages = hiera_array('profile::baseconfig::packages')
 
   # Install a range of useful tools.
-  package { $basepackages :
-    ensure => 'present',
-  }
-
+  ensure_packages ( $basepackages, {
+    'ensure' => 'present',
+  })
 }
